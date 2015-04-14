da0abfdf5a8d95dac5f20b80c9be78203eb58551
nitinsingla29 <nitinsingla29@gmail.com>
2015-04-14 17:22:37 +0530

    Tested HelloWorld using annotation based configuration

--- HelloWorldWithAnnotation/src/main/java/com/sample/init/Main.java
+++ HelloWorldWithAnnotation/src/main/java/com/sample/init/Main.java
@@ -1,14 +1,25 @@
 package com.sample.init;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+@Configuration
 public class Main {
+	
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"SpringBeans.xml");
 
-		HelloWorld obj = (HelloWorld) context.getBean("helloBean");
-		obj.printHello();
+		HelloWorld helloWorld = (HelloWorld) context.getBean(HelloWorld.class);
+		helloWorld.setName("Nitin");
+		helloWorld.printHello();
+	}
+
+	@Bean
+	public HelloWorld helloWorld() {
+		return new HelloWorld();
 	}
 }
