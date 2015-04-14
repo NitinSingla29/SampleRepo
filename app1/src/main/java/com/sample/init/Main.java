373057b74f4286fe3cf36c25ffaba0f98223f881
nitinsingla29 <nitinsingla29@gmail.com>
2015-04-14 16:32:29 +0530

    Tested Hello World Application

--- app1/src/main/java/com/sample/init/Main.java
+++ app1/src/main/java/com/sample/init/Main.java
@@ -3,11 +3,11 @@ package com.sample.init;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class App {
+public class Main {
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext(
 				"SpringBeans.xml");
- 
+
 		HelloWorld obj = (HelloWorld) context.getBean("helloBean");
 		obj.printHello();
 	}
